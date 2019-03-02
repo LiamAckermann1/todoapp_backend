f6cb830ce09e2dbbb5a98fa3da40bc195c8f2f93
Liam Ackermann <liamackermann1@gmail.com>
2019-03-02 19:41:52 +0000

    can now click on list items

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,19 @@ class TodoController < ApplicationController
     def index
     end 
     def show
-        @todo_description = "Make Dinner"
-        @todo_time_estimate = 4
+        todo_id = params[:id]
+        if todo_id == '1'
+            @todo_description = "Finish College Application Essay"
+            @todo_time_estimate = 60
+            elsif todo_id == '2'
+            @todo_description = "Walk Dog"
+            @todo_time_estimate = 30 
+            elsif todo_id == '3'
+            @todo_description = "Clean dishes"
+            @todo_time_estimate = 20 
+            elsif todo_id == '4'
+            @todo_description = "Do Laundry"
+            @todo_time_estimate = 15
+        end 
     end 
 end
